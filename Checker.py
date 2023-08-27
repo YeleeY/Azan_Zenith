adaf7d6499d756ceff37e8e38aef079d600538b0
YeleeY <fuzail2108@gmail.com>
2023-08-27 13:00:36 -0500

    Simplified code

--- Checker.py
+++ Checker.py
@@ -8,6 +8,7 @@ subprocess.run(["python", "media player.py"])
 while True:
     current_time = time.strftime("%H:%M")
 
-    if str(current_time) == str(fajr) or str(current_time) == str(zuhr) or str(current_time) == str(asr) or str(current_time) == str(magrib) or str(current_time) == str(isha):
+    if str(current_time) == str(zuhr) or str(current_time) == str(asr) or str(current_time) == str(magrib) or str(current_time) == str(isha):
          
-       subprocess.run(["python", "media player.py"])
+      	 subprocess.run(["mpv", "Azan.mp3"])
+         
--- media player.py
+++
@@ -1,5 +0,0 @@
-from playsound import playsound
-
-audio_file =  "Azan (Hijaz).mp3"
-azan = AudioSegment.from_file(audio_file)
-play(azan)
